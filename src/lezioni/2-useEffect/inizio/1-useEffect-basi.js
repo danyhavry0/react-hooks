f8bf832f2252144fc63eb3d9efb84cc57bc3db40
danyhavry0 <105102223+danyhavry0@users.noreply.github.com>
2024-02-10 15:27:45 +0100

    Finito 2° capitolo

--- src/lezioni/2-useEffect/inizio/1-useEffect-basi.js
+++ src/lezioni/2-useEffect/inizio/1-useEffect-basi.js
@@ -1,9 +1,30 @@
-import React from "react";
+import { Button } from "bootstrap";
+import React, {useEffect} from "react";
 
 const useEffectBasi = () => {
+  console.log("Componente rendering");
+  const [value, setValue] = React.useState(0);
+
+  const add  = () => {
+    setValue((oldValue) => oldValue + 1);
+  };
+
+  useEffect(() => {
+    console.log("Eccomi, sono use Effect");
+    if (value < 1) {
+      document.title = `Nessun Messaggio`;
+    } else {
+      document.title = `Nuovo Messaggi: ${value}`;
+    }
+    return () => console.log("clean up function chiamata");
+  }, [value]);
+
   return (
-    <div>
-      <h4>UseEffect Componente Base</h4>
+    <div className="item">
+      <h4>Value: <span>{value}</span></h4>
+      <button className="button" onClick={add}>
+        Aumenta
+      </button>
     </div>
   );
 };
--- src/lezioni/2-useEffect/inizio/2-useEffect-cleanup.js
+++ src/lezioni/2-useEffect/inizio/2-useEffect-cleanup.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const CleanUp = () => {
+  const [size, setSize] = useState(window.innerWidth) 
+
+  function setWindow() {
+    setSize(window.innerWidth);
+  }
+
+  console.log("Render");
+  
+  useEffect(() => {
+    console.log("Setto addEventListener");
+    window.addEventListener('resize', setWindow);
+    // return () => {
+    //   console.log("Rimuovo addEventListener");
+    //   window.removeEventListener('resize', setWindow);
+    // }
+  }, []);
+
   return (
-    <div>
-      <h1> CleanUp Componente </h1>
+    <div className="container w-75 col-6 bg-white shadow py-4" style={{textAlign: 'center'}}>
+      <h1> {size} </h1>
     </div>
   );
 };
